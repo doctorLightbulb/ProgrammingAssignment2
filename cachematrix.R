e18607afb33fb60bbee062571c43977678ee3b6b
Doctor I. Q. Lightbulb <64180446+doctorLightbulb@users.noreply.github.com>
2021-05-05 11:49:34 -0700

    Clarified Variables
    Changed variable "m" to "cacheMValues".

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,16 @@
 ## These functions cache and return the inverse of a matrix object.
 
-## This function stores a matrix.
+## This function stores a matrix whose values can be retrieved.
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
+    cacheMValues <- NULL
     set <- function(y) {
         x <<- y
-        m <<- NULL
+        cacheMValues <<- NULL
     }
     get <- function() x
-    setmatrix <- function(matrix) m <<- matrix
-    getmatrix <- function() m
+    setmatrix <- function(matrix) cacheMValues <<- matrix
+    getmatrix <- function() cacheMValues
     list(set = set, get = get,
          setmatrix = setmatrix,
          getmatrix = getmatrix)
@@ -19,16 +19,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## And this function calculates the inverse of
 ## the matrix stored in makeCacheMatrix(). 
-## NOTE: This functions works only with an invertible matrix.
+## NOTE: This function works only with an invertible matrix.
 
 cacheSolve <- function(x, ...) {
-    m <- x$getmatrix()
-    if(!is.null(m)) {
+    cacheMValues <- x$getmatrix()
+    if(!is.null(cacheMValues)) {
         message("getting cached data")
-        return(m)
+        return(cacheMValues)
     }
     data <- x$get()
-    m <- solve(data, ...)
-    x$setmatrix(m)
-    m
+    cacheMValues <- solve(data, ...)
+    x$setmatrix(cacheMValues)
+    cacheMValues
 }
